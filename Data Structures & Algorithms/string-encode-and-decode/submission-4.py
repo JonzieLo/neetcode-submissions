b11238305d288552af5dcec1806d1d7d1b1e0415
Jonathan Lo <40780561+JonzieLo@users.noreply.github.com>
2026-05-22 23:30:25 +0800

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        import json
+        return json.dumps(strs)
+    def decode(self, s: str) -> List[str]:
+        import json
+        return json.loads(s)
